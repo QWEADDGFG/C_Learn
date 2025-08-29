fe707a6a3ff869a5bf39b8c0c17d2601b8cedbb2
QWEADDGFG <231060023@hdu.edu.cn>
2025-08-29 14:29:48 +0800

    add some test code 20250827

--- src/1_fopen.c
+++ src/1_fopen.c
@@ -1,6 +1,17 @@
 #include "fun.h"
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[])
+{
     // ./1_fopen.c file1
-    ARGS_CHECK(argc,2);
+    // if(argc!=2){
+    //     fprintf(stderr,"args error!\n");
+    //     return -1;
+    // }
+    ARGS_CHECK(argc, 2);
+    FILE *fp = fopen(argv[1], "r+");
+    // if (fp == NULL){
+    //     perror("fopen 1");
+    //     return -1;
+    // }
+    ERROR_CHECK(fp, NULL, "guopeng");
     return 0;
 }
